7d67c2583238b5aaa26fc885f1f9ebb9769836c9
Molly Saleh <Molly@kdvi-macbook-pro-01.home>
2024-04-05 21:57:29 +0100

    got basics of the game working, number inputs into the selected box, turns red if wrong and blue if right

--- src/main.ts
+++ src/main.ts
@@ -5,26 +5,61 @@ if (!selectChosenBox || !selectChosenNumber) {
   throw new Error("Issue With Selector");
 }
 
+let box = "";
 let boxId = "";
+let boxValue = "";
+let boxHTML = "";
 let numberId = "";
 
-const handleBoxSelector = (event: Event) => {
-  let boxId = event.target;
-  return boxId;
-};
 
 
+// const handleBoxSelector = (event: Event) => {
+//   let boxAsElement = event.target as HTMLInputElement
+//   boxId = String(boxAsElement.getAttribute('id'))
+//   if (boxId === numberId) {
+//     console.log("hey there")
+//   }
+// };
 
-const handleNumberSelector = (event: Event) => {
-  let numberId = event.target;
-  return numberId;
+selectChosenBox.forEach((box) => {
+  boxId = "" ;
+  box.addEventListener("click", () => {
+    boxId = String(box.getAttribute("id"));
+    boxValue = boxId.slice(1)
+    let boxInnerText = box.innerHTML
+    console.log(boxId);
+    checkMatch();
+    })
+  })
+
+
+selectChosenNumber.forEach((number) => {
+  number.addEventListener("click", () => {
+    numberId = String(number.getAttribute("id"));
+    console.log(numberId);
+    checkMatch();
+  });
+});
+
+const checkMatch = () => {
+  if (boxValue === numberId) {
+    let newHTML = document.getElementById(boxId)
+    newHTML.innerHTML = numberId
+    newHTML.style.color = 'blue'
+    console.log(newHTML)
+  } else {
+    let newHTML = document.getElementById(boxId)
+    newHTML.innerHTML = numberId
+    newHTML.style.color = 'red'
+    console.log(newHTML)
+  }
 };
 
 // event listeners
-selectChosenBox.forEach((box) => {
-  box.addEventListener("click", handleBoxSelector);
-});
+// selectChosenBox.forEach((box) => {
+//   box.addEventListener("click", handleBoxSelector);
+// });
 
-selectChosenNumber.forEach((number) => {
-  number.addEventListener("click", handleNumberSelector);
-});
+// selectChosenNumber.forEach((number) => {
+//   number.addEventListener("click", handleNumberSelector);
+// });
