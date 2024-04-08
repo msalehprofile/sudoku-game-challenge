62867c9e6cc282d64d34f7c7c79319d1ebcb6f15
Molly Saleh <Molly@192.168.1.190>
2024-04-08 17:24:45 +0100

    got the highlighting system working

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import countdown from 'time-span'
+import countdown from "time-span";
 
 const selectChosenBox = document.querySelectorAll(".grid__box__no");
 const selectChosenNumber = document.querySelectorAll(".numbers__single");
@@ -8,36 +8,40 @@ if (!selectChosenBox || !selectChosenNumber || !errorCount) {
   throw new Error("Issue With Selector");
 }
 
+console.log(selectChosenBox)
+
+
 
 let boxId = "";
 let boxValue = "";
 let numberId = "";
-let newHTML = "";
+let inputtedNumber = "";
+let boxHorizontalClass = "";
+let boxVerticalClass = "";
+let boxtotalClass = "";
 
-let countdown = require('time-counter'),
-    log = console.log.bind(console);
 
-let countUpTimer = new Timer();
-countUpTimer.on('change', log);
+// let countdown = require('time-counter'),
+//     log = console.log.bind(console);
 
-// const handleBoxSelector = (event: Event) => {
-//   let boxAsElement = event.target as HTMLInputElement
-//   boxId = String(boxAsElement.getAttribute('id'))
-//   if (boxId === numberId) {
-//     console.log("hey there")
-//   }
-// };
+// let countUpTimer = new Timer();
+// countUpTimer.on('change', log);
 
 selectChosenBox.forEach((box) => {
-  boxId = "";
   box.addEventListener("click", () => {
     numberId = " ";
     boxId = String(box.getAttribute("id"));
+    boxVerticalClass = String(box.getAttribute("class")).slice(3, 5);
+    boxHorizontalClass = String(box.getAttribute("class")).slice(0, 2);
+    boxtotalClass = String(box.getAttribute("class")).slice(6, 8);
+    box.getAttribute('class')
+
     boxValue = boxId.slice(1);
-    let boxInnerText = box.innerHTML;
-    console.log(boxId);
+    console.log("this is the boxclass", boxVerticalClass);
+    console.log("this is the boxid", boxId);
     console.log("this is the numberid", numberId);
     checkMatch();
+    handleHighlightSystem();
   });
 });
 
@@ -49,27 +53,34 @@ selectChosenNumber.forEach((number) => {
   });
 });
 
+
+const handleHighlightSystem = () => {
+  const allBoxesElementGrab = document.getElementsByClassName("grid__box__no");
+  for (let i = 0; i < allBoxesElementGrab.length; i++) {
+    // if the classes contain the combination of rows and columns selected in event listener, make background grey, otherwise make background white
+    if (allBoxesElementGrab[i].classList.contains(boxHorizontalClass) || allBoxesElementGrab[i].classList.contains(boxtotalClass) ||allBoxesElementGrab[i].classList.contains(boxVerticalClass)) {
+      allBoxesElementGrab[i].style.backgroundColor = "grey";
+    } else allBoxesElementGrab[i].style.backgroundColor = "white";
+  }
+};
+
 const checkMatch = () => {
   if (boxValue === numberId) {
-    newHTML = document.getElementById(boxId);
-    newHTML.innerHTML = numberId;
-    newHTML.style.color = "blue";
-    console.log(newHTML);
+    inputtedNumber = document.getElementById(boxId);
+    inputtedNumber.innerHTML = numberId;
+    inputtedNumber.style.color = "blue";
+    console.log(inputtedNumber);
   } else if (numberId === " " || boxId === " ") {
     errorCount.innerHTML = errorCount.innerHTML;
   } else if (boxValue != numberId) {
-    newHTML = document.getElementById(boxId);
-    newHTML.innerHTML = numberId;
-    newHTML.style.color = "red";
-    errorCount.innerHTML = String(Number(errorCount.innerHTML) + 1)
+    inputtedNumber = document.getElementById(boxId);
+    inputtedNumber.innerHTML = numberId;
+    inputtedNumber.style.color = "red";
+    errorCount.innerHTML = String(Number(errorCount.innerHTML) + 1);
   }
 };
 
 // event listeners
-// selectChosenBox.forEach((box) => {
-//   box.addEventListener("click", handleBoxSelector);
-// });
-
-// selectChosenNumber.forEach((number) => {
-//   number.addEventListener("click", handleNumberSelector);
-// });
+selectChosenBox.forEach((box) => {
+  box.addEventListener("click", handleHighlightSystem);
+});
